564d422f55868f3861597d211d1654109bb8749a
xentic-ti <antonio.sanchez@xentic.com.pe>
2025-05-26 21:23:15 -0500

    Primer commit del proyecto SPFx

---
+++ src/webparts/botonPopup/components/IBotonPopupProps.ts
@@ -0,0 +1,7 @@
+export interface IBotonPopupProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/botonPopup/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IBotonPopupWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'BotonPopupWebPartStrings' {
+  const strings: IBotonPopupWebPartStrings;
+  export = strings;
+}
